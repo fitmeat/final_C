694c4a02966e7179a4ba8263bd1c69d6178562cf
fitmeat <eenot825@gmail.com>
2022-12-28 20:30:48 +0300

    Выполнил задание

--- Program.cs
+++ Program.cs
@@ -1,9 +1,29 @@
-﻿Console.Clear();
-Console.WriteLine("Введите размер массива: ");
+﻿void NewString(string[] oldString, string[] newString)
+{
+    int count = 0;
+    for (int i = 0; i < oldString.Length; i++)
+    {
+            if (oldString[i].Length <= 3)
+            {
+                newString[count] = oldString[i];
+                count++;
+            }
+    }
+    Console.WriteLine($"{string.Join(" ", newString)}");
+}
+
+
+Console.Clear();
+Console.WriteLine("Введите размер массива строк: ");
 int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значения массива строк: ");
 string[] str1 = new string[n];
 for (int i = 0; i < n; i++)
 {
-    str1[i] = Console.ReadLine();    
+    str1[i] = Console.ReadLine();
 }
- Console.WriteLine($"{string.Join(", ", str1)}");
+Console.WriteLine("Начальный массив:");
+Console.WriteLine($"{string.Join(" ", str1)}");
+string[] str2 = new string[n];
+Console.WriteLine("Конечный массив:");
+NewString(str1, str2);
