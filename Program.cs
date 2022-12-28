1410f42ef99e809eb941bf0887cfde24365d126c
fitmeat <eenot825@gmail.com>
2022-12-28 19:16:13 +0300

    Создал и заполнил строку

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("Введите размер массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string[] str1 = new string[n];
+for (int i = 0; i < n; i++)
+{
+    str1[i] = Console.ReadLine();    
+}
+ Console.WriteLine($"{string.Join(", ", str1)}");
